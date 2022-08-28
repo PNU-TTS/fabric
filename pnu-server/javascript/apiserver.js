80854a01643e14dc2a1d60a89ef11c9210495d56
rubinstory@naver.com <rubinstory@naver.com>
2022-08-28 19:16:17 +0900

    Add executing transaction api

--- pnu-server/javascript/apiserver.js
+++ pnu-server/javascript/apiserver.js
@@ -172,6 +172,45 @@ app.post('/create/transaction/', async function (req, res) {
 });
 
 /**
+ * REC 매수 API
+ * 
+ * @method  POST
+ * 
+ * @param id        Transaction ID
+ * @param buyer     구매자 ID
+ */
+ app.post('/executeTransaction/', async function (req, res) {
+    try {
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+        console.log(`CCP path: ${ccpPath}`);
+
+        const identity = await wallet.get(req.body.userID);
+        if (!identity) {
+            res.status(401).json({error: `An identity for the user "${req.body.userID}" does not exist in the wallet`});
+        }
+        const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));        
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: req.body.userID, discovery: { enabled: true, asLocalhost: true } });
+
+        const network = await gateway.getNetwork('rec-trade-channel');
+        const contract = network.getContract('pnucc');
+
+        await contract.submitTransaction('executeTransaction', 
+            req.body.id,
+            req.body.buyer
+        )
+
+        res.status(200).json({response: "Successfully executed transaction"});
+        await gateway.disconnect();
+        
+    } catch (error) {
+        res.status(500).json({error: error});
+    }
+});
+
+/**
  * 사용자 등록 API
  * 
  * @param departmentName    buyer 또는 supplier   
