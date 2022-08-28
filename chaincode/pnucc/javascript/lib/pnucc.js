46b9045542bd47ee31ce21469c3ff843d8d38ebe
rubinstory@naver.com <rubinstory@naver.com>
2022-08-28 16:22:50 +0900

    feature: Add new api
    - 거래 내역을 조회하는 API를 새로 추가했습니다.
        - 체결된 거래 내역과 미체결된 거래 내역을 불러올 수 있습니다.
    - 거래 내역에 등록 날짜와 체결 날짜를 추가했습니다.
    
    - 체인코드의 변경된 명세에 대응할 수 있도록 새로운 API를 추가했습니다.

--- chaincode/pnucc/javascript/lib/pnucc.js
+++ chaincode/pnucc/javascript/lib/pnucc.js
@@ -8,7 +8,13 @@
 
 const { Contract } = require('fabric-contract-api');
 
-function Transaction(id, target, price, quantity, supplier, buyer) {
+function printMethodEntry(functionName) { console.info('========= START: ' + functionName + ' =========') }
+function printMethodExit(functionName) { console.info('========= FINISH: ' + functionName + ' E =========') }
+
+function Transaction(
+    id, target, price, quantity, 
+    supplier, buyer, 
+    registeredDate, executedDate) {
     this.id = id;
     
     this.target = target;
@@ -17,61 +23,77 @@ function Transaction(id, target, price, quantity, supplier, buyer) {
 
     this.supplier = supplier;
     this.buyer = buyer;
-}
 
-function printMethodEntry(functionName) {
-    console.info('========= START: ' + functionName + ' =========')
-}
-
-function printMethodExit(functionName) {
-    console.info('========= FINISH: ' + functionName + ' E =========')
+    this.registeredDate = registeredDate;
+    this.executedDate = executedDate;
 }
 
 class PnuCC extends Contract {
 
-    isDataValid(byteData) {
-        return byteData && byteData.length != 0;
+    isDataValid(byteData) { return byteData && byteData.length != 0; }
+
+    async initLedger(ctx) { 
+        printMethodEntry('Initialize Ledger'); 
+        this.NEXT_TRANSACTION_ID = 1;
+        
+        for await (const {key, value} of ctx.stub.getStateByRange('', '')) {
+            const strValue = Buffer.from(value).toString('utf8');
+            let transaction;
+            try {
+                transaction = JSON.parse(strValue);
+                this.NEXT_TRANSACTION_ID += 1;
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        printMethodEntry(`Registered Transactions Amount: ${this.NEXT_TRANSACTION_ID}`); 
     }
 
-    async initLedger(ctx) {
-        console.info('============= Initialize Ledger ===========');
-    }
-
-    async createNewTransaction(ctx, id, target, price, quantity, supplier, buyer) {
-        printMethodEntry(`Create New Transaction ID: ${id}`);
+    // REC 매도 등록
+    async createNewTransaction(ctx, target, price, quantity, supplier) {
+        printMethodEntry('Create New Transaction');
 
         let transactionAsBytes = await ctx.stub.getState(id);
+
         if (this.isDataValid(transactionAsBytes)) {
             throw new Error(`Certificate with ${id} already exist`);
         }
 
+        const currentDateTime = new Date();
         const transaction = new Transaction(
-            id = id,
-            target = target,
-            price = price,
-            quantity = quantity,
-            supplier = supplier,
-            buyer = buyer
+            id = this.NEXT_TRANSACTION_ID, 
+            target = target, price = price, quantity = quantity,
+            supplier = supplier, buyer = null,
+            registeredDate = parseInt(currentDateTime.getTime() / 1000),
+            executedDate = null
         );
+
+        this.NEXT_TRANSACTION_ID += 1;
+
         console.log(`${id}, ${price}`);
         console.log(`${JSON.stringify(transaction)}`);
+        
         await ctx.stub.putState(id, Buffer.from(JSON.stringify(transaction)));
 
-        printMethodExit(`Create New Transaction ID: ${id}`);
+        printMethodExit('Create New Transaction ID');
     }
 
     async executeTransaction(ctx, id, buyer) {
         printMethodEntry(`Execute Transaction ID: ${id}`);
 
         let transactionAsBytes = await ctx.stub.getState(id);
+
         if (!this.isDataValid(transactionAsBytes)) {
             throw new Error(`Certificate with ${id} does not exist`);
         }
         
         const transaction = JSON.parse(transactionAsBytes);
         transaction.buyer = buyer;
-        await ctx.stub.putState(id, Buffer.from(JSON.stringify(transaction)));
+        const currentDateTime = new Date();
+        transaction.executedDate = parseInt(currentDateTime.getTime() / 1000);
 
+        await ctx.stub.putState(id, Buffer.from(JSON.stringify(transaction)));
         printMethodExit(`Execute Transaction ID: ${id}`);
     }
 
@@ -89,24 +111,61 @@ class PnuCC extends Contract {
 
     async queryAllTransactions(ctx) {
         printMethodEntry('Query All Transactions');
-
-        const startKey = '';
-        const endKey = '';
         const allResults = [];
         
-        for await (const {key, value} of ctx.stub.getStateByRange(startKey, endKey)) {
+        for await (const {key, value} of ctx.stub.getStateByRange('', '')) {
             const strValue = Buffer.from(value).toString('utf8');
-            let record;
+            let transaction;
             try {
-                record = JSON.parse(strValue);
+                transaction = JSON.parse(strValue);
+                allResults.push({ Transaction: transaction });
             } catch (err) {
                 console.log(err);
-                record = strValue;
             }
-            allResults.push({ Key: key, Record: record });
         }
 
-        //allResults.push({ Key: "key", Record: "recordYJ" });
+        printMethodExit('Query All Transactions');
+        return JSON.stringify(allResults);
+    }
+
+    async queryUnexecutedTransactions(ctx) {
+        printMethodEntry('Query Unexecuted Transactions');
+        const allResults = [];
+        
+        for await (const {key, value} of ctx.stub.getStateByRange('', '')) {
+            const strValue = Buffer.from(value).toString('utf8');
+            let transaction;
+            try {
+                transaction = JSON.parse(strValue);
+                if (transaction.executedDate == null) {
+                    allResults.push({ Transaction: transaction });
+                }
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        printMethodExit('Query All Transactions');
+        return JSON.stringify(allResults);
+    }
+
+    async queryExecutedTransactions(ctx) {
+        printMethodEntry('Query Unexecuted Transactions');
+        const allResults = [];
+        
+        for await (const {key, value} of ctx.stub.getStateByRange('', '')) {
+            const strValue = Buffer.from(value).toString('utf8');
+            let transaction;
+            try {
+                transaction = JSON.parse(strValue);
+                if (transaction.executedDate != null) {
+                    allResults.push({ Transaction: transaction });
+                }
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
         printMethodExit('Query All Transactions');
         return JSON.stringify(allResults);
     }
--- pnu-server/javascript/apiserver.js
+++ pnu-server/javascript/apiserver.js
@@ -10,44 +10,11 @@ app.use(bodyParser.json());
 const { FileSystemWallet, Gateway } = require('fabric-network');
 const path = require('path');
 const ccpPath = path.resolve(__dirname, '..', '..', 'pnu-network', 'organizations', 'peerOrganizations', 'rec-client-peer-org.pnu.com', 'connection-rec-client-peer-org.json');
+function printSystemLog(functionName) { console.info('========= ' + functionName + ' =========') }
 
-function printSystemLog(functionName) {
-    console.info('========= ' + functionName + ' =========')
-}
-
-// curl http://localhost:8080/query/allTransactions/
-app.get('/query/allTransactions/', async function (req, res) {
-    try {
-        const walletPath = path.join(process.cwd(), 'wallet');
-        const wallet = await Wallets.newFileSystemWallet(walletPath);
-        console.log(`Wallet path: ${walletPath}`);
-        console.log(`CCP path: ${ccpPath}`);
-
-        const identity = await wallet.get('appUser');
-        if (!identity) {
-            console.log('An identity for the user "appUser" does not exist in the wallet');
-            console.log('Run the registerUser.js application before retrying');
-            return;
-        }
-        const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
-        
-        const gateway = new Gateway();
-        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
-        printSystemLog('Connect Success');
-
-        const network = await gateway.getNetwork('rec-trade-channel');
-        const contract = network.getContract('pnucc');
-
-        const result = await contract.evaluateTransaction('queryAllTransactions');
-        printSystemLog('API Success');
-
-        res.status(200).json({response: result.toString()});
-    } catch (error) {
-        res.status(500).json({error: error});
-    }
-});
-
-// curl /query/transaction/2
+/**
+ * REC 거래 ID 값을 기반으로 조회하는 API
+ */
 app.get('/query/transaction/:transactionId', async function (req, res) {
     try {
         const walletPath = path.join(process.cwd(), 'wallet');
@@ -57,28 +24,120 @@ app.get('/query/transaction/:transactionId', async function (req, res) {
 
         const identity = await wallet.get('appUser');
         if (!identity) {
-            console.log('An identity for the user "appUser" does not exist in the wallet');
-            console.log('Run the registerUser.js application before retrying');
-            return;
+            res.status(401).json({error: 'An identity for the user "appUser" does not exist in the wallet'});
         }
+
         const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
-        
         const gateway = new Gateway();
         await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
-        printSystemLog('Connect Success');
 
         const network = await gateway.getNetwork('rec-trade-channel');
         const contract = network.getContract('pnucc');
 
         const result = await contract.evaluateTransaction('queryTransactionById', req.params.transactionId);
-        printSystemLog('API Success');
         res.status(200).json({response: result.toString()});
     } catch (error) {
         res.status(500).json({error: error});
     }
 });
 
-// curl -d '{"userID": "cyj", "txID": "1", "target": "target", "price": 50000, "quantity": 300, "supplier": "supp", "buyer": "cyj"}' -H "Content-Type: application/json" -X POST http://localhost:8080/create/transaction/
+/**
+ * 모든 거래 내역을 조회하는 API
+ */
+ app.get('/query/allTransactions/', async function (req, res) {
+    try {
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+        console.log(`CCP path: ${ccpPath}`);
+
+        const identity = await wallet.get('appUser');
+        if (!identity) {
+            res.status(401).json({error: 'Run the registerUser.js application before retrying'});
+        }
+        const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
+
+        const network = await gateway.getNetwork('rec-trade-channel');
+        const contract = network.getContract('pnucc');
+
+        const result = await contract.evaluateTransaction('queryAllTransactions');
+
+        res.status(200).json({response: result.toString()});
+    } catch (error) {
+        res.status(500).json({error: error});
+    }
+});
+
+/**
+ * 미체결된 거래 내역을 조회하는 API
+ */
+ app.get('/queryUnexecutedTransactions/', async function (req, res) {
+    try {
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+        console.log(`CCP path: ${ccpPath}`);
+
+        const identity = await wallet.get('appUser');
+        if (!identity) {
+            res.status(401).json({error: 'An identity for the user "appUser" does not exist in the wallet'});
+        }
+
+        const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
+
+        const network = await gateway.getNetwork('rec-trade-channel');
+        const contract = network.getContract('pnucc');
+
+        const result = await contract.evaluateTransaction('queryUnexecutedTransactions');
+        res.status(200).json({response: result.toString()});
+    } catch (error) {
+        res.status(500).json({error: error});
+    }
+});
+
+/**
+ * 체결된 거래 내역을 조회하는 API
+ */
+ app.get('/query/executedTransactions/', async function (req, res) {
+    try {
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+        console.log(`CCP path: ${ccpPath}`);
+
+        const identity = await wallet.get('appUser');
+        if (!identity) {
+            res.status(401).json({error: 'An identity for the user "appUser" does not exist in the wallet'});
+        }
+
+        const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
+
+        const network = await gateway.getNetwork('rec-trade-channel');
+        const contract = network.getContract('pnucc');
+
+        const result = await contract.evaluateTransaction('queryExecutedTransactions');
+        res.status(200).json({response: result.toString()});
+    } catch (error) {
+        res.status(500).json({error: error});
+    }
+});
+
+/**
+ * REC 매도 등록 API
+ * 
+ * @method  POST
+ * 
+ * @param target    Django에 등록된 REC ID 값
+ * @param price     REC 개당 가격
+ * @param quantity  REC 개수
+ * @param supplier  REC 공급자
+ */
 app.post('/create/transaction/', async function (req, res) {
     try {
         const walletPath = path.join(process.cwd(), 'wallet');
@@ -88,37 +147,36 @@ app.post('/create/transaction/', async function (req, res) {
 
         const identity = await wallet.get(req.body.userID);
         if (!identity) {
-            console.log(`An identity for the user "${req.body.userID}" does not exist in the wallet`);
-            console.log('Run the registerUser.js application before retrying');
-            return;
+            res.status(401).json({error: `An identity for the user "${req.body.userID}" does not exist in the wallet`});
         }
-        const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
-        
+        const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));        
         const gateway = new Gateway();
         await gateway.connect(ccp, { wallet, identity: req.body.userID, discovery: { enabled: true, asLocalhost: true } });
-        printSystemLog('Connect Success');
 
         const network = await gateway.getNetwork('rec-trade-channel');
         const contract = network.getContract('pnucc');
 
         await contract.submitTransaction('createNewTransaction', 
-            req.body.txID,
             req.body.target,
             req.body.price,
             req.body.quantity,
             req.body.supplier,
-            req.body.buyer
         )
-        printSystemLog('API Success');
+
         res.status(200).json({response: "Successfully created transaction"});
         await gateway.disconnect();
+        
     } catch (error) {
         res.status(500).json({error: error});
     }
 });
 
-
-// curl -d '{"enrollmentID": "cyj", "departmentName": "buyer"}' -H "Content-Type: application/json" -X POST http://localhost:8080/register/
+/**
+ * 사용자 등록 API
+ * 
+ * @param departmentName    buyer 또는 supplier   
+ * @param enrollmentID      등록할 사용자 ID
+ */
 app.post('/register/', async function (req, res) {
     try {
         const walletPath = path.join(process.cwd(), 'wallet');
@@ -128,28 +186,21 @@ app.post('/register/', async function (req, res) {
 
         const identity = await wallet.get(req.body.enrollmentID);
         if (identity) {
-            console.log(`An identity for the user "${req.body.enrollmentID}" already exist in the wallet`);
-            return;
+            res.status(406).json({error: `"${req.body.enrollmentID}" already exist in the wallet`});
         }
+
         const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
-        
-        // Create a new CA client for interacting with the CA.
         const caURL = ccp.certificateAuthorities['ca.rec-client-peer-org.pnu.com'].url;
         const ca = new FabricCAServices(caURL);
 
-        // Check to see if we've already enrolled the admin user.
         const adminIdentity = await wallet.get('admin');
         if (!adminIdentity) {
-            console.log('An identity for the admin user "admin" does not exist in the wallet');
-            console.log('Run the enrollAdmin.js application before retrying');
-            return;
+            res.status(400).json({error: 'An identity for the admin user "admin" does not exist in the wallet'});
         }
 
-        // build a user object for authenticating with the CA
         const provider = wallet.getProviderRegistry().getProvider(adminIdentity.type);
         const adminUser = await provider.getUserContext(adminIdentity, 'admin');
 
-        // Register the user, enroll the user, and import the new identity into the wallet.
         const secret = await ca.register({
             affiliation: `rec-client-peer-org.${req.body.departmentName}`,
             enrollmentID: req.body.enrollmentID,
@@ -169,10 +220,10 @@ app.post('/register/', async function (req, res) {
             mspId: 'RecClientPeerOrgMSP',
             type: 'X.509',
         };
+
         await wallet.put(req.body.enrollmentID, x509Identity);
-        
-        printSystemLog('API Success');
         res.status(200).json({response: `Successfully register "${req.body.enrollmentID}" and imported it into the wallet`});
+
     } catch (error) {
         res.status(500).json({error: error});
     }
