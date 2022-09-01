ff5569b87bad81498a2a9484170888f30779344c
cyj893 <cyj89317@gmail.com>
2022-09-01 23:55:17 +0900

    feat: Add more parameters for changed form to register 2 peers

--- pnu-network/organizations/ccp-generate.sh
+++ pnu-network/organizations/ccp-generate.sh
@@ -5,24 +5,26 @@ function one_line_pem {
 }
 
 function json_ccp {
-    local PP=$(one_line_pem $5)
-    local CP=$(one_line_pem $6)
+    local PP=$(one_line_pem $6)
+    local CP=$(one_line_pem $7)
     sed -e "s/\${ORG}/$1/" \
         -e "s/\${ORGDOMAIN}/$2/" \
         -e "s/\${P0PORT}/$3/" \
-        -e "s/\${CAPORT}/$4/" \
+        -e "s/\${P1PORT}/$4/" \
+        -e "s/\${CAPORT}/$5/" \
         -e "s#\${PEERPEM}#$PP#" \
         -e "s#\${CAPEM}#$CP#" \
         organizations/ccp-template.json
 }
 
 function yaml_ccp {
-    local PP=$(one_line_pem $5)
-    local CP=$(one_line_pem $6)
+    local PP=$(one_line_pem $6)
+    local CP=$(one_line_pem $7)
     sed -e "s/\${ORG}/$1/" \
         -e "s/\${ORGDOMAIN}/$2/" \
         -e "s/\${P0PORT}/$3/" \
-        -e "s/\${CAPORT}/$4/" \
+        -e "s/\${P1PORT}/$4/" \
+        -e "s/\${CAPORT}/$5/" \
         -e "s#\${PEERPEM}#$PP#" \
         -e "s#\${CAPEM}#$CP#" \
         organizations/ccp-template.yaml | sed -e $'s/\\\\n/\\\n        /g'
@@ -31,19 +33,21 @@ function yaml_ccp {
 ORG=ManagementPeerOrg
 ORGDOMAIN=management-peer-org
 P0PORT=7051
+P1PORT=8051
 CAPORT=7054
 PEERPEM=organizations/peerOrganizations/management-peer-org.pnu.com/tlsca/tlsca.management-peer-org.pnu.com-cert.pem
 CAPEM=organizations/peerOrganizations/management-peer-org.pnu.com/ca/ca.management-peer-org.pnu.com-cert.pem
 
-echo "$(json_ccp $ORG $ORGDOMAIN $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/management-peer-org.pnu.com/connection-management-peer-org.json
-echo "$(yaml_ccp $ORG $ORGDOMAIN $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/management-peer-org.pnu.com/connection-management-peer-org.yaml
+echo "$(json_ccp $ORG $ORGDOMAIN $P0PORT $P1PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/management-peer-org.pnu.com/connection-management-peer-org.json
+echo "$(yaml_ccp $ORG $ORGDOMAIN $P0PORT $P1PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/management-peer-org.pnu.com/connection-management-peer-org.yaml
 
 ORG=RecClientPeerOrg
 ORGDOMAIN=rec-client-peer-org
 P0PORT=9051
+P1PORT=10051
 CAPORT=8054
 PEERPEM=organizations/peerOrganizations/rec-client-peer-org.pnu.com/tlsca/tlsca.rec-client-peer-org.pnu.com-cert.pem
 CAPEM=organizations/peerOrganizations/rec-client-peer-org.pnu.com/ca/ca.rec-client-peer-org.pnu.com-cert.pem
 
-echo "$(json_ccp $ORG $ORGDOMAIN $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/rec-client-peer-org.pnu.com/connection-rec-client-peer-org.json
-echo "$(yaml_ccp $ORG $ORGDOMAIN $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/rec-client-peer-org.pnu.com/connection-rec-client-peer-org.yaml
+echo "$(json_ccp $ORG $ORGDOMAIN $P0PORT $P1PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/rec-client-peer-org.pnu.com/connection-rec-client-peer-org.json
+echo "$(yaml_ccp $ORG $ORGDOMAIN $P0PORT $P1PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/rec-client-peer-org.pnu.com/connection-rec-client-peer-org.yaml
